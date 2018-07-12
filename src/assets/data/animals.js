e707c69239461a3db3ab5a2bcdcaa4639191cb9b
anastminsk <anast-minsk@yandex.ru>
2018-07-12 13:52:38 +0300

    game/feature: add tasks constants

---
+++ src/assets/data/animals.js
@@ -0,0 +1,42 @@
+export const ANIMALS = [
+  {
+    name: ['badger', 'барсук'],
+    className: 'badger'
+  },
+  {
+    name: ['bear', 'медведь'],
+    className: 'bear'
+  },
+  {
+    name: ['beaver', 'бобр'],
+    className: 'beaver'
+  },
+  {
+    name: ['fox', 'лис', 'лиса', 'лисица'],
+    className: 'fox'
+  },
+  {
+    name: ['hedgehog', 'еж'],
+    className: 'hedgehog'
+  },
+  {
+    name: ['moose', 'лось'],
+    className: 'moose'
+  },
+  {
+    name: ['mouse', 'мышь'],
+    className: 'mouse'
+  },
+  {
+    name: ['rabbit', 'кролик', 'заяц'],
+    className: 'rabbit'
+  },
+  {
+    name: ['squirrel', 'белка'],
+    className: 'squirrel'
+  },
+  {
+    name: ['wolf', 'волк'],
+    className: 'wolf'
+  }
+]
---
+++ src/assets/data/cities.js
@@ -0,0 +1,46 @@
+export const CITIES = [
+  {
+    city: ['berlin', 'берлин'],
+    className: 'berlin'
+  },
+  {
+    city: ['london', 'лондон'],
+    className: 'london'
+  },
+  {
+    city: ['minsk', 'минск'],
+    className: 'minsk'
+  },
+  {
+    city: ['moscow', 'москва'],
+    className: 'moscow'
+  },
+  {
+    city: ['new york', 'нью-йорк'],
+    className: 'new-york'
+  },
+  {
+    city: ['paris', 'париж'],
+    className: 'paris'
+  },
+  {
+    city: ['praha', 'прага'],
+    className: 'praha'
+  },
+  {
+    city: ['rome', 'рим'],
+    className: 'rome'
+  },
+  {
+    city: ['saint petersburg', 'санкт-петербург'],
+    className: 'saint-petersburg'
+  },
+  {
+    city: ['warsaw', 'варшава'],
+    className: 'warsaw'
+  },
+  {
+    city: ['wien', 'вена'],
+    className: 'wien'
+  }
+]

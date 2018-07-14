0957c3268b24439a06cb372961ec38dd94f37cc6
anastminsk <anast-minsk@yandex.ru>
2018-07-14 18:02:25 +0300

    game/feature: add loading component

---
+++ src/components/loading/index.js
@@ -0,0 +1,37 @@
+class Loading {
+  constructor() {
+    this.homeScreen = document.getElementById('home-screen');
+    this.navigation = document.getElementById('navigation');
+    this.scoreSection = document.getElementById('score-section');
+    this.scoreContainer = document.getElementById('score-container');
+    this.startButton = document.getElementById('start');
+    this.scoreButton = document.getElementById('score');
+    this.closeButton = document.getElementById('close-button');
+    this.startButton.addEventListener('click', () => this.startGame());
+    this.scoreButton.addEventListener('click', () => this.showScore());
+    this.closeButton.addEventListener('click', () => this.handleScreen());
+  }
+
+  init(score) {
+    this.score = score;
+  }
+
+  startGame() {
+    this.heroCreationScreen = document.getElementById('hero-creation-screen');
+    this.heroCreationScreen.classList.remove('hidden');
+    this.homeScreen.classList.add('hidden');
+  }
+
+  showScore() {
+    this.score.showScore(this.scoreContainer);
+    this.scoreSection.classList.remove('hidden');
+    this.navigation.classList.add('hidden');
+  }
+
+  handleScreen() {
+    this.scoreSection.classList.add('hidden');
+    this.navigation.classList.remove('hidden');
+  }
+}
+
+export default Loading;
